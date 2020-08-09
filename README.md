e4884b56eb1f71bb188525d4e9cce393df188650
Alex Willmer <alex@moreati.org.uk>
2020-08-09 16:53:28 +0100

    First stab at rich.progress support to the rich callback prototype

--- README.md
+++ README.md
@@ -5,3 +5,9 @@
 Rather than print a new line for each failure of a retried task, print on a 
 single line until the task fails or succeeds.
 
+## Rich
+
+Use [rich] progress meters to render Ansible progress. Unlike the `overprint`
+callback, this handles multiple hosts.
+
+[rich]: https://rich.readthedocs.io
--- rich/callback_plugins/rich.py
+++ rich/callback_plugins/rich.py
@@ -7,6 +7,7 @@ from ansible.plugins.callback.default import CallbackModule as DefaultCallbackMo
 
 import rich.console
 import rich.padding
+import rich.progress
 
 
 DOCUMENTATION = '''
@@ -32,6 +33,8 @@ class CallbackModule(CallbackBase):
             highlight=False,
             markup=False,  # Don't interpret [square brackets] as styling
         )
+        self._progress = None
+        self._progress_tasks = {}
         self._play = None
 
     def print(self, msg, indent=0, **kwargs):
@@ -45,10 +48,29 @@ class CallbackModule(CallbackBase):
         self.print('PLAY [{name}]'.format(name=name), style='bold')
 
     def v2_playbook_on_task_start(self, task, is_conditional):
+        self._progress = rich.progress.Progress(
+            '{task.description}',
+            '{task.percentage:>3.1f}%',
+            console=self._console,
+        )
         name = task.get_name().strip()
         self.print('TASK [{name}]'.format(name=name), indent=2, style='bold')
 
+    def v2_runner_on_start(self, host, task):
+        progress_task = self._progress.add_task(host.name, total=task.retries)
+        self._progress_tasks[(host.name, task._uuid)] = progress_task
+        self._progress.start()
+
+    def v2_runner_retry(self, result):
+        #retries = result._result['retries']
+        #attempts = result._result['attempts']
+        progress_task = self._progress_tasks[(result._host.name, result._task._uuid)]
+        self._progress.update(progress_task, advance=1)
+        #task_name = result.task_name or result._task
+
     def v2_runner_on_failed(self, result, ignore_errors=False):
+        if self._progress.finished:
+            self._progress.stop()
         hostname = result._host.get_name()
         delegated_vars = result._result.get('_ansible_delegated_vars', None)
         if delegated_vars:
