17c62d4e3e80028fb91a2122b03ec4f9d41b0c84
Alex Willmer <alex@moreati.org.uk>
2021-03-07 21:31:59 +0000

    rich: Flesh out progress bars, add playbook stats

--- rich/callback_plugins/rich.py
+++ rich/callback_plugins/rich.py
@@ -1,13 +1,27 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
+import logging
 
-from ansible.plugins.callback import CallbackBase
-from ansible.plugins.callback.default import CallbackModule as DefaultCallbackModule
+from ansible import constants as C
+from ansible import context
+from ansible.errors import AnsibleAssertionError
+try:
+    # Ansible >= 2.10.x
+    from ansible_collections.community.general.plugins.callback.yaml import CallbackModule as YAMLCallBackModule
+except ImportError:
+    # Ansible <= 2.9.x
+    from ansible.plugins.callback.yaml import CallbackModule as YAMLCallBackModule
+from ansible.utils.color import colorize, hostcolor
+from ansible.utils.display import Display, color_to_log_level, logger
 
+import rich.align
+import rich.box
 import rich.console
 import rich.padding
 import rich.progress
+import rich.table
+import rich.theme
 
 
 DOCUMENTATION = '''
@@ -19,64 +33,228 @@ DOCUMENTATION = '''
   short_description: rich Ansible screen output
   description:
     - Displays ansible screen out using Rich
+  extends_documentation_fragment:
+    - default_callback
 '''
 
 
-class CallbackModule(CallbackBase):
+SIMPLER = rich.box.Box(
+    '    \n'
+    '    \n'
+    ' ─  \n'
+    '    \n'
+    '    \n'
+    ' ─  \n'
+    '    \n'
+    '    \n'
+)
+
+
+class RichDisplay(Display):
+    def __init__(self, verbosity=0):
+        super().__init__(verbosity=verbosity)
+        self._first_banner = True
+        theme = rich.theme.Theme({
+            'banner': 'bold',
+            'ok': self.color_to_style(C.COLOR_OK),
+            'changed': self.color_to_style(C.COLOR_CHANGED),
+            'unreachable': self.color_to_style(C.COLOR_UNREACHABLE),
+            'failed': self.color_to_style(C.COLOR_ERROR),
+            'skipped': self.color_to_style(C.COLOR_SKIP),
+            'rescued': self.color_to_style(C.COLOR_OK),
+            'ignored': self.color_to_style(C.COLOR_WARN),
+            'table.header': 'default',
+            'table.footer': 'default',
+        })
+        self.console = rich.console.Console(
+            emoji=False,
+            highlight=False,
+            markup=False,  # Don't interpret [square brackets] as styling
+            theme=theme,
+        )
+        self.error_console = rich.console.Console(
+            emoji=False,
+            highlight=False,
+            markup=False,  # Don't interpret [square brackets] as styling
+            theme=theme,
+            stderr=True,
+        )
+
+    @staticmethod
+    def color_to_style(color, default='default'):
+        """Convert an Ansible color spec to a rich style.
+        """
+        if color is None:
+            return default
+        return color.replace('bright', 'bold')
+
+    def display(self, msg, color=None, stderr=False, screen_only=False, log_only=False, newline=True, **kwargs):
+        style = self.color_to_style(color)
+        console = self.error_console if stderr else self.console
+        end = '\n' if newline and not msg.endswith('\n') else ''
+
+        if not log_only:
+            console.print(msg, style=style, end=end, **kwargs)
+
+        if logger and not screen_only:
+            log_level = logging.INFO
+            if color:
+                try:
+                    log_level = color_to_log_level[color]
+                except KeyError:
+                    # this should not happen, but JIC
+                    raise AnsibleAssertionError(f'Invalid color supplied to display: {color}')
+            logger.log(log_level, msg)
+
+    def banner(self, msg, color=None):
+        msg = msg.strip()
+        style = 'banner' if color is None else self.color_to_style(color)
+        if self._first_banner:
+            self._first_banner = False
+            leader = ''
+        else:
+            leader = '\n'
+        self.console.print(f"{leader}{msg}", style=style)
+
+
+class CallbackModule(YAMLCallBackModule):
     CALLBACK_NAME = 'rich'
     CALLBACK_TYPE = 'stdout'
     CALLBACK_VERSION = 2.0
 
     def __init__(self):
-        self._console = rich.console.Console(
-            emoji=False,
-            highlight=False,
-            markup=False,  # Don't interpret [square brackets] as styling
-        )
         self._progress = None
         self._progress_tasks = {}
-        self._play = None
+        super().__init__()
+        self._display = RichDisplay()
 
-    def print(self, msg, indent=0, **kwargs):
-        msg = rich.padding.Padding.indent(msg, indent)
-        return self._console.print(msg, **kwargs)
-
-    def v2_playbook_on_play_start(self, play):
-        #self._play = play
-        name = play.get_name().strip()
-        
-        self.print('PLAY [{name}]'.format(name=name), style='bold')
+    @staticmethod
+    def host_spec(result):
+        hostname = result._host.get_name()
+        delegated_vars = result._result.get('_ansible_delegated_vars')
+        if delegated_vars:
+            delegated_hostname = delegated_vars['ansible_host']
+            return f"{hostname} -> {delegated_hostname}"
+        else:
+            return f"{hostname}"
 
     def v2_playbook_on_task_start(self, task, is_conditional):
-        self._progress = rich.progress.Progress(
-            '{task.description}',
-            '{task.percentage:>3.1f}%',
-            console=self._console,
-        )
-        name = task.get_name().strip()
-        self.print('TASK [{name}]'.format(name=name), indent=2, style='bold')
+        if task.until:
+            self._progress = rich.progress.Progress(
+                '{task.description}',
+                '{task.completed} of {task.total}',
+                console=self._display.console,
+                auto_refresh=False,
+                transient=True,
+            )
+        super().v2_playbook_on_task_start(task, is_conditional)
 
     def v2_runner_on_start(self, host, task):
-        progress_task = self._progress.add_task(host.name, total=task.retries)
-        self._progress_tasks[(host.name, task._uuid)] = progress_task
-        self._progress.start()
+        if task.until:
+            progress_task = self._progress.add_task(host.name, total=task.retries)
+            self._progress_tasks[(host.name, task._uuid)] = progress_task
+            self._progress.start()
 
     def v2_runner_retry(self, result):
-        #retries = result._result['retries']
-        #attempts = result._result['attempts']
         progress_task = self._progress_tasks[(result._host.name, result._task._uuid)]
         self._progress.update(progress_task, advance=1)
-        #task_name = result.task_name or result._task
+        self._progress.refresh()
+
+    def v2_runner_on_ok(self, result):
+        if self._progress:
+            self._progress.stop()
+            self._progress.refresh()
+
+        super().v2_runner_on_ok(result)
 
     def v2_runner_on_failed(self, result, ignore_errors=False):
-        if self._progress.finished:
+        if self._progress:
             self._progress.stop()
-        hostname = result._host.get_name()
-        delegated_vars = result._result.get('_ansible_delegated_vars', None)
-        if delegated_vars:
-            host_spec = '{hostname} -> {delegated_host}'.format(hostname=hostname, delegated_host=delegated_vars['ansible_host'])
-        else:
-            host_spec = hostname
-        self._clean_results(result._result, result._task.action)
-        result = result._result
-        self.print('fatal: [{host_spec}]: FAILED! => {result}'.format(host_spec=host_spec, result=result), indent=4, style='red')
+            self._progress.refresh()
+
+        super().v2_runner_on_failed(result, ignore_errors)
+
+    def v2_playbook_on_stats(self, stats):
+        self._display.banner('PLAY RECAP')
+
+        def cell(v, style, align='right'):
+            if v:
+                return rich.align.Align(str(v), align=align, style=style)
+            else:
+                return rich.align.Align(str(v), align=align, style='dim')
+
+        def stat_column(header, field, style):
+            total = sum(field.values())
+            return rich.table.Column(
+                header=rich.align.Align(header, align='center'),
+                footer=cell(total, style=style)
+            )
+
+        table = rich.table.Table(
+            rich.table.Column(header='Host', footer='Total'),
+            stat_column('OK', stats.ok, style='ok'),
+            stat_column('Changed', stats.changed, style='changed'),
+            stat_column('Unreachable', stats.dark, style='unreachable'),
+            stat_column('Failed', stats.failures, style='failed'),
+            stat_column('Skipped', stats.skipped, style='skipped'),
+            stat_column('Rescued', stats.rescued, style='rescued'),
+            stat_column('Ignored', stats.ignored, style='ignored'),
+            rich.table.Column(header='Total'),
+            box=SIMPLER,
+            padding=(0, 0, 0, 0),  # Top, Right, Bottom, Left
+            pad_edge=False,
+            show_edge=False,
+            show_footer=True,
+        )
+
+        overall_total = 0
+        for h in sorted(stats.processed.keys()):
+            t = stats.summarize(h)
+            host_total = sum(t.values())
+            overall_total += host_total
+            table.add_row(
+                h,
+                cell(t['ok'], style='ok'),
+                cell(t['changed'], style='changed'),
+                cell(t['unreachable'], style='unreachable'),
+                cell(t['failures'], style='failed'),
+                cell(t['skipped'], style='skipped'),
+                cell(t['rescued'], style='rescued'),
+                cell(t['ignored'], style='ignored'),
+                cell(host_total, style='dim default'),
+            )
+
+            self._display.display(
+                u"%s : %s %s %s %s %s %s %s" % (
+                    hostcolor(h, t, False),
+                    colorize(u'ok', t['ok'], None),
+                    colorize(u'changed', t['changed'], None),
+                    colorize(u'unreachable', t['unreachable'], None),
+                    colorize(u'failed', t['failures'], None),
+                    colorize(u'skipped', t['skipped'], None),
+                    colorize(u'rescued', t['rescued'], None),
+                    colorize(u'ignored', t['ignored'], None),
+                ),
+                log_only=True
+            )
+        table.columns[-1].footer = cell(overall_total, style='dim default')
+        self._display.console.print(table)
+
+        # print custom stats if required
+        if stats.custom and self.show_custom_stats:
+            self._display.banner("CUSTOM STATS: ")
+            # per host
+            # TODO: come up with 'pretty format'
+            for k in sorted(stats.custom.keys()):
+                if k == '_run':
+                    continue
+                self._display.display('\t%s: %s' % (k, self._dump_results(stats.custom[k], indent=1).replace('\n', '')))
+
+            # print per run custom stats
+            if '_run' in stats.custom:
+                self._display.display("", screen_only=True)
+                self._display.display('\tRUN: %s' % self._dump_results(stats.custom['_run'], indent=1).replace('\n', ''))
+            self._display.display("", screen_only=True)
+
+        if context.CLIARGS['check'] and self.check_mode_markers:
+            self._display.banner("DRY RUN")
